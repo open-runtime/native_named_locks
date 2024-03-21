77ddf7b4ffc96d2568eb570c4766006db92e254f
Tsavo Knott <tsavo@pieces.app>
2024-03-21 19:05:47 -0400

    chore: Small Cleanup to primitives.dart imports.

--- lib/primitives.dart
+++ lib/primitives.dart
@@ -1,9 +1,9 @@
 library;
 
-import 'dart:ffi';
-import 'dart:io';
+import 'dart:ffi' show Finalizable, NativeType, Pointer, nullptr;
+import 'dart:io' show Platform;
 import 'package:ffi/ffi.dart' show calloc;
-import 'package:runtime_native_named_locks/errors.dart';
+import 'package:runtime_native_named_locks/errors.dart' show NamedLockErrors;
 import 'src/bindings/windows.dart'
     show
         CloseHandle,
@@ -29,6 +29,9 @@ sealed class NamedLock<H extends NativeType, M extends NativeType> implements Fi
 
   late final M mutex_handle;
 
+  int get address => throw UnimplementedError(
+      "Class Property Getter 'address' in Sealed Abstract Class 'NamedLock' is Unimplemented.");
+
   NamedLock._({required String name}) : this.name = name;
 
   factory NamedLock({required String name}) => Platform.isWindows
@@ -42,12 +45,11 @@ sealed class NamedLock<H extends NativeType, M extends NativeType> implements Fi
       throw UnimplementedError("Class Member 'void lock()' in Sealed Abstract Class 'NamedLock' is Unimplemented.");
 
   void unlock() =>
-      throw UnimplementedError("Class Member 'void lock()' in Sealed Abstract Class 'NamedLock' is Unimplemented.");
+      throw UnimplementedError("Class Member 'void unlock()' in Sealed Abstract Class 'NamedLock' is Unimplemented.");
 
   void dispose() =>
-      throw UnimplementedError("Class Member 'void _dispose()' in Sealed Abstract Class 'NamedLock' is Unimplemented.");
+      throw UnimplementedError("Class Member 'void dispose()' in Sealed Abstract Class 'NamedLock' is Unimplemented.");
 
-  int get address => throw UnimplementedError();
-
-  String toString() => throw UnimplementedError();
+  String toString() =>
+      throw UnimplementedError("Class Member 'toString()' in Sealed Abstract Class 'NamedLock' is Unimplemented.");
 }
