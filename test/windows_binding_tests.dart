912186d506a3bad24017b80d74fe703befd4b076
Tsavo Knott <tsavo@pieces.app>
2024-03-29 17:16:26 -0400

    test: Testing of Windows FFI Bindings

---
+++ test/windows_binding_tests.dart
@@ -0,0 +1,79 @@
+@TestOn('windows')
+
+import 'dart:ffi';
+import 'dart:io';
+
+import 'package:ffi/ffi.dart';
+import 'package:path/path.dart' show join;
+import 'package:runtime_native_named_locks/src/bindings/windows.dart'
+    show CreateMutexW, ReleaseMutex, WAIT_ABANDONED, WAIT_OBJECT_0, WAIT_TIMEOUT;
+import 'package:test/test.dart'
+    show TestOn, contains, equals, expect, group, isA, isNonZero, isNotNull, isNull, isTrue, tearDown, test, throwsA;
+import 'package:win32/win32.dart' show CloseHandle, DWORD, GetLastError, HANDLE, INFINITE, NULL, WaitForSingleObject;
+import 'package:windows_foundation/internal.dart'
+    show BoolConversions, HString, ObjectConversion, getRestrictedErrorDescription;
+
+void main() {
+  group('WindowsNamedLock', () {
+    test('Initialize ', () {
+      final identifier = join("Global\\", 'my_named_lock');
+
+      // An LPCWSTR is a 32-bit pointer to a CONSTANT string of 16-bit Unicode characters, which MAY be null-terminated.
+      // typedef const wchar_t* LPCWSTR;
+      // the equivalent of a char pointer (`const wchar_t*`) in C code.
+      // StringUtf16Pointer
+      final Pointer<Utf16> native_LPCWSTR = identifier.toNativeUtf16(allocator: malloc);
+      // final Uint32 native_BOOL = Uint32.fromBool(initial_owner);
+
+      // BOOL
+      // final native_BOOL = initial_owner.toNative();
+
+      // final HString native_string = HString.fromString(identifier);
+      // print(native_string.handle);
+
+      // final native_boolean = false();
+
+      // HANDLE CreateMutexW(
+      //     [in, optional] LPSECURITY_ATTRIBUTES lpMutexAttributes,
+      //     [in]           BOOL                  bInitialOwner,
+      //     [in, optional] LPCWSTR               lpName
+      // );
+
+      // A BOOL is a 32-bit field that is set to 1 to indicate TRUE, or 0 to indicate FALSE.
+      // This type is declared as follows:
+      // typedef int BOOL, *PBOOL, *LPBOOL;
+      print("=================================== CREATE MUTEX W ==================================== \n");
+      final int mutex_address = CreateMutexW(nullptr, 0, native_LPCWSTR);
+
+      if (mutex_address == nullptr) {
+        print("=================================== GET LAST ERROR ==================================== \n");
+        int native_last_error = GetLastError();
+        String? error_message = getRestrictedErrorDescription(native_last_error);
+        print('Error: ${error_message}');
+      }
+
+      print("=================================== WAIT FOR SINGLE OBJECT ==================================== \n");
+      final HANDLE = Pointer.fromAddress(mutex_address);
+      print(HANDLE.address);
+
+      final result = WaitForSingleObject(HANDLE.address, INFINITE);
+
+      print(result);
+      print('$INFINITE, $WAIT_OBJECT_0, $WAIT_ABANDONED, $WAIT_TIMEOUT');
+
+      print("=================================== RELEASE MUTEX ==================================== \n");
+      print('ReleaseMutex');
+      final released = ReleaseMutex(HANDLE.address);
+      print('0 is false and 1 is true, 0 is even and 1 is odd');
+      print(released.isOdd);
+
+      print("=================================== CLOSE HANDLE ==================================== \n");
+      final closed = CloseHandle(HANDLE.address);
+      print('0 is false and 1 is true, 0 is even and 1 is odd');
+      print(closed.isOdd);
+
+      print("=================================== FREE ==================================== \n");
+      malloc.free(native_LPCWSTR);
+    });
+  });
+}
